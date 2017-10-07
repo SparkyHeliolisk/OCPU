00f85d22d996b0287e6a5fdc9de8d2a4934a74e3
zellman01 <zellman01@gmail.com>
2017-10-07 14:30:01 -0400

    Add two users for sysop

--- users.js
+++ users.js
@@ -528,7 +528,7 @@ class User {
 	 * Special permission check for system operators
 	 */
 	hasSysopAccess() {
-		if (this.isSysop && Config.backdoor || Config.OCPUbackdoor && ['joltsjolteon'].includes(this.userid)) {
+		if (this.isSysop && Config.backdoor || Config.OCPUbackdoor && ['joltsjolteon', 'sparkyheliolisk', 'alfastorm'].includes(this.userid)) {
 			// This is the Pokemon Showdown system operator backdoor.
 
 			// Its main purpose is for situations where someone calls for help, and
