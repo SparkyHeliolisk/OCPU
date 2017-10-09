1e5ae8970fa91f5e45eb3504260ceb6dc53323ec
Insist <DeathlyPlays@users.noreply.github.com>
2017-10-09 10:04:56 -0500

    Fix merging conflicts

--- chat.js
+++ chat.js
@@ -147,11 +147,7 @@ class PatternTester {
 	 * @param {string[]} elems
 	 */
 	register(...elems) {
-<<<<<<< HEAD
 		for (const elem of elems) {
-=======
-		for (let elem of elems) {
->>>>>>> Restart all files
 			this.elements.push(elem);
 			if (/^[^ ^$?|()[\]]+ $/.test(elem)) {
 				this.fastElements.add(this.fastNormalize(elem));
@@ -561,17 +557,10 @@ class CommandContext {
 	sendModCommand(data) {
 		this.room.sendModCommand(data);
 	}
-<<<<<<< HEAD
 	privateModCommand(data, logOnlyText) {
 		this.room.sendModCommand(data);
 		this.logEntry(data);
 		this.room.modlog(data + (logOnlyText || ""));
-=======
-	privateModCommand(data) {
-		this.room.sendModCommand(data);
-		this.logEntry(data);
-		this.room.modlog(data);
->>>>>>> Restart all files
 	}
 	globalModlog(action, user, text) {
 		let buf = "(" + this.room.id + ") " + action + ": ";
@@ -866,12 +855,7 @@ class CommandContext {
 		let tags = html.toLowerCase().match(/<\/?(div|a|button|b|strong|em|i|u|center|font|marquee|blink|details|summary|code|table|td|tr)\b/g);
 		if (tags) {
 			let stack = [];
-<<<<<<< HEAD
 			for (const tag of tags) {
-=======
-			for (let i = 0; i < tags.length; i++) {
-				let tag = tags[i];
->>>>>>> Restart all files
 				if (tag.charAt(1) === '/') {
 					if (!stack.length) {
 						this.errorReply("Extraneous </" + tag.substr(2) + "> without an opening tag.");
@@ -999,11 +983,7 @@ Chat.loadPlugins = function () {
 	// info always goes first so other plugins can shadow it
 	Object.assign(commands, require('./chat-plugins/info').commands);
 
-<<<<<<< HEAD
 	for (const file of FS('chat-plugins/').readdirSync()) {
-=======
-	for (let file of FS('chat-plugins/').readdirSync()) {
->>>>>>> Restart all files
 		if (file.substr(-3) !== '.js' || file === 'info.js') continue;
 		const plugin = require(`./chat-plugins/${file}`);
 
@@ -1162,15 +1142,9 @@ Chat.parseText = function (str) {
 			continue;
 		}
 
-<<<<<<< HEAD
 		for (const formattingResolver of formattingResolvers) {
 			let start = formattingResolver.token;
 			let end = formattingResolver.endToken || start;
-=======
-		for (let f = 0; f < formattingResolvers.length; f++) {
-			let start = formattingResolvers[f].token;
-			let end = formattingResolvers[f].endToken || start;
->>>>>>> Restart all files
 
 			if (stack.length && end.startsWith(token) && str.substr(i, end.length) === end && output[stack.length].replace(token, '').length) {
 				for (let j = stack.length - 1; j >= 0; j--) {
@@ -1182,11 +1156,7 @@ Chat.parseText = function (str) {
 						}
 
 						let str = output.pop();
-<<<<<<< HEAD
 						let outstr = formattingResolver.resolver(str.trim());
-=======
-						let outstr = formattingResolvers[f].resolver(str.trim());
->>>>>>> Restart all files
 						if (!outstr) outstr = `${start}${str}${end}`;
 						output[stack.length - 1] += outstr;
 						i += end.length;
@@ -1237,13 +1207,8 @@ Chat.getDataPokemonHTML = function (template, gen = 7) {
 	buf += '<span class="col pokemonnamecol" style="white-space:nowrap"><a href="https://pokemonshowdown.com/dex/pokemon/' + template.id + '" target="_blank">' + template.species + '</a></span> ';
 	buf += '<span class="col typecol">';
 	if (template.types) {
-<<<<<<< HEAD
 		for (const type of template.types) {
 			buf += `<img src="https://play.pokemonshowdown.com/sprites/types/${type}.png" alt="${type}" height="14" width="32">`;
-=======
-		for (let i = 0; i < template.types.length; i++) {
-			buf += `<img src="https://play.pokemonshowdown.com/sprites/types/${template.types[i]}.png" alt="${template.types[i]}" height="14" width="32">`;
->>>>>>> Restart all files
 		}
 	}
 	buf += '</span> ';
