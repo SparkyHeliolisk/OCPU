9f419d78f47179893d4bdef8f3b48d7dc57dabdf
zellman01 <zellman01@gmail.com>
2017-10-19 21:38:19 -0400

    Update lockdown code

--- rooms.js
+++ rooms.js
@@ -732,7 +732,7 @@ class GlobalRoom {
 			}
 		});
 		Users.users.forEach(u => {
-			u.send(`|pm|~OCPU Server|/raw <div class="broadcast-red"><b>The server is restarting soon.</b><br />Please finish your battles quickly. No new battles can be started until the server resets in a few minutes.</div>`);
+			u.send(`|pm|~OCPU Server|${u.group}${u.name}|/raw <div class="broadcast-red"><b>The server is restarting soon.</b><br />Please finish your battles quickly. No new battles can be started until the server resets in a few minutes.</div>`);
 		});
 
 		this.lockdown = true;
