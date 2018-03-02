b7fc31411f2f04335e78b33f9e327f628c5401c8
zellman01 <zellman01@gmail.com>
2018-03-02 08:32:01 -0500

    Delete duplicate function

--- chat-plugins/ocpu.js
+++ chat-plugins/ocpu.js
@@ -129,36 +129,6 @@ function parseStatus(text, encoding) {
 	return text;
 }
 
-/*OCPU.regdate = function (target, callback) {
-	target = toId(target);
-	if (regdateCache[target]) return callback(regdateCache[target]);
-	let options = {
-		host: 'pokemonshowdown.com',
-		port: 80,
-		path: '/users/' + target + '.json',
-		method: 'GET',
-	};
-	http.get(options, function (res) {
-		let data = '';
-		res.on('data', function (chunk) {
-			data += chunk;
-		}).on('end', function () {
-			data = JSON.parse(data);
-			let date = data['registertime'];
-			if (date !== 0 && date.toString().length < 13) {
-				while (date.toString().length < 13) {
-					date = Number(date.toString() + '0');
-				}
-			}
-			if (date !== 0) {
-				regdateCache[target] = date;
-				saveRegdateCache();
-			}
-			callback((date === 0 ? false : date));
-		});
-	});
-};*/
-
 function load() {
 	fs.readFile('config/customcolors.json', 'utf8', function (err, file) {
 		if (err) return;
@@ -921,7 +891,7 @@ exports.commands = {
 		    "- " + OCPU.nameColor('SparkyHeliolisk', true) + " (Sysop, Creative Admin, Development)<br />" +
 		    "- " + OCPU.nameColor('AlfaStorm', true) + " (Sysop, Technical Admin, Development)<br />" +
 		    "<br />" +
-			 "<u>Contributors</u<<br />" +
+			 "<u>Contributors</u><br />" +
 			 "- " + OCPU.nameColor('Glameowch', true) + "(Events Admin)<br />" +
 			 "<br />" +
 		    "<u>Development:</u><br />" +
